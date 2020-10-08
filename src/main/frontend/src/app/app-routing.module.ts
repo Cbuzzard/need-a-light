7a0ee4c2f3a962b6b7ed7d07d1f9fafe25dce182
cbuzzard <corwin1998@gmail.com>
2020-10-07 23:25:34 -0400

    admin route

--- src/main/frontend/src/app/app-routing.module.ts
+++ src/main/frontend/src/app/app-routing.module.ts
@@ -1,13 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AdminComponent } from './components/admin/admin.component';
 import { HomeComponent } from './components/home/home.component';
 
 const routes: Routes = [
-  { path: '', component: HomeComponent }
+  { path: '', component: HomeComponent },
+  { path: 'admin', component: AdminComponent },
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes,{ scrollPositionRestoration: 'disabled' })],
+  imports: [RouterModule.forRoot(
+    routes,
+    { scrollPositionRestoration: 'enabled',
+    anchorScrolling: 'enabled'})],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
