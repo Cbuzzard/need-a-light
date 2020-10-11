80eb69b91a8512f2e6cb9c34a966659b80abf36b
cbuzzard <corwin1998@gmail.com>
2020-10-10 23:45:47 -0400

    init

---
+++ src/main/java/com/buzzardsview/needalight/data/BlogRepository.java
@@ -0,0 +1,7 @@
+package com.buzzardsview.needalight.data;
+
+import com.buzzardsview.needalight.model.Blog;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface BlogRepository extends JpaRepository<Blog, String> {
+}
