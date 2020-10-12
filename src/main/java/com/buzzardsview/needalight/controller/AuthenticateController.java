16bcc7df54b0fd3ce7c93af069cfd873b84d032d
cbuzzard <corwin1998@gmail.com>
2020-10-11 23:48:52 -0400

    isAdmin endpoint

--- src/main/java/com/buzzardsview/needalight/controller/AuthenticateController.java
+++ src/main/java/com/buzzardsview/needalight/controller/AuthenticateController.java
@@ -1,5 +1,8 @@
 package com.buzzardsview.needalight.controller;
 
+import com.buzzardsview.needalight.data.UserRepository;
+import com.buzzardsview.needalight.model.User;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,6 +15,9 @@ import static com.buzzardsview.needalight.security.AppTokenProvider.getUserFromT
 @RequestMapping("rest/authenticate")
 public class AuthenticateController {
 
+    @Autowired
+    private UserRepository userRepository;
+
     @GetMapping("")
     public boolean authenticateUser(HttpServletRequest request) {
 
@@ -19,6 +25,19 @@ public class AuthenticateController {
         try {
             userFromToken = getUserFromToken(request);
             return userFromToken.isPresent();
+        } catch (Throwable err) {
+            return false;
+        }
+    }
+
+    @GetMapping("admin")
+    public boolean authenticateAdmin(HttpServletRequest request) {
+
+        Optional<String> userFromToken;
+        try {
+            userFromToken = getUserFromToken(request);
+            User user = userRepository.getByGoogleId(userFromToken.get()).orElseThrow();
+            return user.isAdmin();
         } catch (Throwable err) {
             return false;
         }
