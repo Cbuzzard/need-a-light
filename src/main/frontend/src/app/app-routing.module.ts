ea3de1a7403bf8c5f17ec1e93949d03b0cc11a01
cbuzzard <corwin1998@gmail.com>
2020-10-11 23:51:41 -0400

    blog routes

--- src/main/frontend/src/app/app-routing.module.ts
+++ src/main/frontend/src/app/app-routing.module.ts
@@ -1,18 +1,30 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AboutComponent } from './components/about/about.component';
 import { AdminComponent } from './components/admin/admin.component';
+import { BlogListComponent } from './components/blog-list/blog-list.component';
+import { BlogComponent } from './components/blog/blog.component';
+import { ContactComponent } from './components/contact/contact.component';
+import { GalleryComponent } from './components/gallery/gallery.component';
 import { HomeComponent } from './components/home/home.component';
+import { PricingComponent } from './components/pricing/pricing.component';
 
 const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'admin', component: AdminComponent },
+  { path: 'blog/:slug', component: BlogComponent},
+  { path: 'blog', component: BlogListComponent},
+  { path: 'pricing', component: PricingComponent},
+  { path: 'contact', component: ContactComponent},
+  { path: 'about', component: AboutComponent},
+  { path: 'gallery', component: GalleryComponent},
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(
     routes,
     { scrollPositionRestoration: 'enabled',
-    anchorScrolling: 'enabled'})],
+    })],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
